6c399c5757f62c319f9d3aa7cfa7c6e61b82a309
Sethunath Raveendran <sethunath3@gmail.com>
2019-11-26 16:16:13 +0530

    generic singleton

---
+++ Assets/Project/Scripts/Connection/ConnectionService.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PocketTank.Generics;
+using SocketIO;
+// using Quobject.SocketIoClientDotNet.Client;
+
+namespace PocketTank.Connection
+{
+    public class ConnectionService : GenericMonoSingleton<ConnectionService>
+    {
+        [SerializeField]
+        private SocketIOComponent connectorPrefab;
+        private SocketIOComponent connectorObject;
+        void Start()
+        {
+            if(connectorObject == null)
+            {
+                connectorObject = GameObject.Instantiate(connectorPrefab);
+                connectorObject.transform.parent = gameObject.transform;
+                Debug.Log("connecting.....");
+                connectorObject.On ("CONNECTION_ESTABLISHED", (SocketIOEvent ev) => {
+                    Dictionary<string, string> data = new Dictionary<string, string>();
+                    data["PLAYER_ID"] = PlayerPrefs.GetInt("PLAYER_ID", 0).ToString();
+                    data["PLAYER_NAME"] = PlayerPrefs.GetString("PLAYER_NAME", "SETHUNATH").ToString();
+                    Debug.Log("sending authrequest");
+                    connectorObject.Emit("USER_AUTH_REQUEST", new JSONObject(data));
+                });
+            }
+        }
+
+        void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Generics/GenericMonoSingleton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PocketTank.Generics
+{
+    public class GenericMonoSingleton<T> : MonoBehaviour where T : GenericMonoSingleton<T>
+    {
+        private static T instance;
+        public static T Instance{get{ return instance; }}
+        protected virtual void Awake()
+        {
+            if(instance == null || instance != this)
+            {
+                instance = (T)this;
+            }
+            else
+            {
+                Debug.LogWarning("Duplicate instance is being created");
+                Destroy(this);
+            }
+        }
+    }
+}
